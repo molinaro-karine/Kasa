c70827d782b6ea328089ddf864ab595e6996a97b
molinaro karine <molinaro.karine@hotmail.com>
2022-09-28 09:36:00 +0200

    Ajout carousel-notation

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -33,6 +33,7 @@ export default function Carousel(props) {
   return (
     <div className="carousel">
       {slides}
+      <p className="carrousel-notation">{carousel + 1}/{props.children.length}</p>
     </div>
-  );
+    );
 }
